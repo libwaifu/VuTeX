141d5ea69880011aeabc569f643f9773e0233438
Aster <galaster@foxmail.com>
2019-01-04 16:24:15 +0800

    模组::Predefined mathematical environment

--- vuepress-plugin-math/index.js
+++ vuepress-plugin-math/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = () => ({
     enhanceAppFiles: path.resolve(__dirname, 'src/enhanceApp.js'),
     extendMarkdown: md => {
-        md.use(require('./out/throwMath'))
+        md.use(require('./out/throwMath')),
+        md.use(require('./out/enviroments'))
     },
 })
